739b9c9cadd615c42766d94421fd215ad8a91290
Dolev .H <33119992+Dolev@users.noreply.github.com>
2020-05-27 17:09:05 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -4,8 +4,15 @@
 #include"doctest.h"
 #include <iostream>
 #include "DemoGame.hpp"
-
-
+TEST_CASE("testValue"){
+            CHECK(3+3==6); 
+            CHECK(3*3==9); 
+            CHECK(25/5==5); 
+            CHECK(110-10==100); 
+            CHECK(4*4==16); 
+            CHECK(3*1==3);
+            CHECK(2-1==1);
+};
 TEST_CASE("testtrue"){
             CHECK(true==true); 
             CHECK(true==true); 
@@ -81,7 +88,27 @@ TEST_CASE("testValue"){
             CHECK(2==2);
             CHECK(3==3);
             CHECK(4==4);
-            CHECK(5==);
+            CHECK(5==5);
+            CHECK(6==6);
+            CHECK(7==7);
+            CHECK(8==8);
+            CHECK(9==9);
+            CHECK(0==0);
+            CHECK(1==1);
+            CHECK(2==2);
+            CHECK(3==3);
+            CHECK(4==4);
+            CHECK(5==5);
+            CHECK(6==6);
+            CHECK(7==7);
+            CHECK(8==8);
+            CHECK(9==9);
+            CHECK(0==0);
+            CHECK(1==1);
+            CHECK(2==2);
+            CHECK(3==3);
+            CHECK(4==4);
+            CHECK(5==5);
             CHECK(6==6);
             CHECK(7==7);
             CHECK(8==8);
@@ -101,27 +128,7 @@ TEST_CASE("testValue"){
             CHECK(2==2);
             CHECK(3==3);
             CHECK(4==4);
-            CHECK(5==);
-            CHECK(6==6);
-            CHECK(7==7);
-            CHECK(8==8);
-            CHECK(9==9);
-            CHECK(0==0);
-            CHECK(1==1);
-            CHECK(2==2);
-            CHECK(3==3);
-            CHECK(4==4);
-            CHECK(5==);
-            CHECK(6==6);
-            CHECK(7==7);
-            CHECK(8==8);
-            CHECK(9==9);
-            CHECK(0==0);
-            CHECK(1==1);
-            CHECK(2==2);
-            CHECK(3==3);
-            CHECK(4==4);
-            CHECK(5==);
+            CHECK(5==5);
             CHECK(6==6);
             CHECK(7==7);
             CHECK(8==8);
