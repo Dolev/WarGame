4fed73adc03c7bd73249b8cfb5eabf615e7641a1
Dolev .H <33119992+Dolev@users.noreply.github.com>
2020-05-27 16:26:30 +0300

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,46 @@
+//
+// Created by dolev on 24/05/2020.
+//
+#include"doctest.h"
+#include <iostream>
+#include "DemoGame.hpp"
+
+TEST_CASE("testtrue"){
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+};
+TEST_CASE("testfalse") {
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+};
+
+ 
+  
