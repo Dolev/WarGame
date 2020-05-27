ee3cd0dff98d505909927f6cf797ad9dfb222c42
Dolev .H <33119992+Dolev@users.noreply.github.com>
2020-05-27 16:32:12 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -4,7 +4,20 @@
 #include"doctest.h"
 #include <iostream>
 #include "DemoGame.hpp"
-
+TEST_CASE("testSum"){
+            CHECK(3+1==2+2); 
+            CHECK(6-1==5); 
+            CHECK(2==1+1); 
+            CHECK(4+10==7*2); 
+            CHECK(15-5==5+5); 
+            CHECK(1==2-1);
+            CHECK(2==3-1);
+            CHECK(4.2-1.1==3.1);
+            CHECK(2.0+0.5==2.5);
+            CHECK(1.5==1.5);
+            CHECK(8*2==16);
+            CHECK(10==10);
+};
 TEST_CASE("testtrue"){
             CHECK(true==true); 
             CHECK(true==true); 
@@ -24,8 +37,24 @@ TEST_CASE("testtrue"){
             CHECK(true==true);
             CHECK(true==true);
             CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true); 
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
+            CHECK(true==true);
 };
 TEST_CASE("testfalse") {
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
+            CHECK(false==false);
             CHECK(false==false);
             CHECK(false==false);
             CHECK(false==false);
